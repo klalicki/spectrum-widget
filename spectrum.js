9eb9cce87c9691762cdc670140e8ae5623793a86
klalicki <32229646+klalicki@users.noreply.github.com>
2021-07-20 20:11:37 -0400

    added some focus styling for labels

--- spectrum.js
+++ spectrum.js
@@ -44,7 +44,7 @@ const presetList = [{
         isRandom: false
     },
     {
-        name: 'preset C',
+        name: 'randomize',
         factors: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 90, 80, 70, 60, 50],
         isRandom: true
     }
